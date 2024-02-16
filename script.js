6aa3bc7e359e0cb7161fe654c050f70a78f87894
DEEPAK RAJORIYA <136314499+DEEPAKRAJORIYA352@users.noreply.github.com>
2024-02-16 20:58:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,22 +8,19 @@ button.addEventListener('click', (e) => {
   let number = Number(input.value);
 
   function isPrime(number) {
-    let arr = [];
+    
     if(number == 1){
-         message.innerHTML = `${number} is a prime number`;
+        message.innerHTML = `${number} is a prime number`;
+        return ;
     }
-     return;
-    for (let i = 1; i <= number; i++) {
-      if ((number%i) == 0) {
-          arr.push(i);
+     
+    for (let i = 2; i < number; i++) {
+      if ((number % i) == 0) {
+          message.innerHTML = `${number} is not a prime number`;
+          return;
       } 
     }
-    if(arr.length == 2){
-      message.innerHTML = `${number} is a prime number`;
-    }
-    else{
-      message.innerHTML = `${number} is not a prime number`;
-    }
+    message.innerHTML = `${number} is a prime number`;
   }
 
   isPrime(number);
