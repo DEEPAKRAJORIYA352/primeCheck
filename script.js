19228fd961d69d78086452ef0cf48d98d8709fbf
DEEPAK RAJORIYA <136314499+DEEPAKRAJORIYA352@users.noreply.github.com>
2024-02-16 20:39:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let input = document.querySelector("#input-box");
+let message = document.querySelector("#message");
+let button = document.querySelector("#check-btn");
+
+button.addEventListener('click', (e) => {
+
+  let number = Number(input.value);
+
+  function isPrime(number) {
+    let arr = [];
+    for (let i = 1; i <= number; i++) {
+      if ((number%i) == 0) {
+          arr.push(i);
+      } 
+    }
+    if(arr.length == 2){
+      message.innerHTML = `${number} is a prime number`;
+    }
+    else{
+      message.innerHTML = `${number} is not a prime number`;
+    }
+  }
+
+  isPrime(number);
+});
